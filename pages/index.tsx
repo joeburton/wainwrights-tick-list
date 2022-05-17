5ca04538bfdce0f2dcf6f33b2876f169c3d4fc6c
Joe Burton <joeburton@gmail.com>
2022-05-17 17:08:27 +0100

    basic pagination of fells

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
 import { gql } from '@apollo/client';
 import client from '../apollo/apollo-client';
 
@@ -8,8 +11,17 @@ import { FellList } from '@/components/index';
 import { FellInterface } from './api/models/Fell';
 
 import styles from '../styles/Home.module.css';
+import React from 'react';
 
 const Home: NextPage<{ fells: [FellInterface] }> = ({ fells }) => {
+  const itemsPerPage = 40;
+  const [page, setPage] = React.useState(1);
+  const [noOfPages] = React.useState(Math.ceil(fells.length / itemsPerPage));
+
+  const handleChange = (_event: any, value: React.SetStateAction<number>) => {
+    setPage(value);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,11 +31,37 @@ const Home: NextPage<{ fells: [FellInterface] }> = ({ fells }) => {
           content="Create your own Wainwrights Tick List"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
       </Head>
       <section>
         <h1>Wainwrights Tick List</h1>
-        <FellList fells={fells} />
       </section>
+      <Stack spacing={2}>
+        <ul>
+          {fells
+            .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+            .map((fell) => {
+              return (
+                <li key={fell.id}>
+                  {fell.name}, m: {fell.metres}, ft: {fell.feet}
+                </li>
+              );
+            })}
+        </ul>
+        <Pagination
+          count={noOfPages}
+          page={page}
+          onChange={handleChange}
+          defaultPage={1}
+          color="primary"
+          size="large"
+          showFirstButton
+          showLastButton
+        />
+      </Stack>
     </div>
   );
 };
